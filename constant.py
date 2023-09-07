67307f73e70f4f4eec8f71f9743304ef0aa55819
ThunderVolt45 <zxc9876zxc@gmail.com>
2023-09-08 00:49:33 +0900

    new file:   JSON/parts_acc.json

--- constant.py
+++ constant.py
@@ -1,8 +1,8 @@
-FILE_PATH_SUBCORE = "./subcore.json"
-FILE_PATH_LEG = "./parts_leg.json"
-FILE_PATH_BODY = "./parts_body.json"
-FILE_PATH_WEAPON = "./parts_weapon.json"
-FILE_PATH_ACC = "./parts_acc.json"
+FILE_PATH_SUBCORE = "./JSON/subcore.json"
+FILE_PATH_LEG = "./JSON/parts_leg.json"
+FILE_PATH_BODY = "./JSON/parts_body.json"
+FILE_PATH_WEAPON = "./JSON/parts_weapon.json"
+FILE_PATH_ACC = "./JSON/parts_acc.json"
 
 LEG = 1
 BODY = 2
